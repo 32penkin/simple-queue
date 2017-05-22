d36d291428dc8e7f8ba4041a0e6ee869b6705fec
Yauhen <penkin.evgeniy@gmail.com>
2017-05-22 15:13:26 +0300

    test (queue): test for "dequeue" method added

--- test/queue.spec.js
+++ test/queue.spec.js
@@ -36,6 +36,42 @@ describe('Queue', () => {
     });
   });
 
+  describe('#dequeue', () => {
+    const qu = new Queue();
+    const qu2 = new Queue();
+
+    qu.enqueue('node1');
+    qu.enqueue('node2');
+    qu.enqueue('node3');
+
+    it('assigns 2 to this.size', () => {
+      qu.dequeue();
+      expect(qu.size).to.equal(2);
+    });
+
+    it('assigns node2 to this.first', () => {
+      expect(qu.first.data).to.equal('node2');
+    });
+
+    it('instanceof', () => {
+      expect(qu.dequeue()).to.be.an.instanceof(Node);
+    });
+
+    it('assigns fully popped stack', () => {
+      qu.dequeue();
+      qu.dequeue();
+      qu.dequeue();
+      qu.dequeue();
+      expect(qu.size).to.equal(0);
+      expect(qu.first).to.equal(null);
+    });
+
+    it('check an empty stack', () => {
+      qu2.dequeue();
+      expect(qu2.size).to.equal(0);
+      expect(qu2.first).to.equal(null);
+    });
+  });
 
 
 });
