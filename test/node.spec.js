be5391e198366b75cc685ef57d6bbe8438f09245
Yauhen <penkin.evgeniy@gmail.com>
2017-05-22 14:51:57 +0300

    test (node, queue): tests for constructors

--- test/node.spec.js
+++ test/node.spec.js
@@ -1,1 +1,27 @@
 const Node = require('../src/node');
+
+describe('Node', () => {
+  describe('#constructor', () => {
+    const node1 = new Node('node1');
+    const node2 = new Node('node2');
+
+    node1.next = node2;
+
+    it('assigns node2 to this.prev', () => {
+      expect(node1.next).to.equal(node2);
+    });
+
+    it('assigns node2.prev to null', () => {
+      expect(node2.next).to.equal(null);
+    });
+
+    it('assigns data of node1 to "node1"', () => {
+      expect(node1.data).to.equal('node1');
+    });
+
+    it('assigns data of node2 to "node2"', () => {
+      expect(node2.data).to.equal('node2');
+    });
+  });
+
+});
--- test/queue.spec.js
+++ test/queue.spec.js
@@ -1,2 +1,17 @@
 const Node = require('../src/node');
 const Queue = require('../src/queue');
+
+describe('Queue', () => {
+  describe('#constructor', () => {
+    const qu = new Queue();
+
+    it('assigns 0 to this.size', () => {
+      expect(qu.size).to.equal(0);
+    });
+
+    it('assigns null to this.head', () => {
+      expect(qu.first).to.equal(null);
+    });
+  });
+
+});
